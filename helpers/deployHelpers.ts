76800617b1e064db02cbf34806c0fd885ccabe35
Brenner Spear <blspear@gmail.com>
2021-09-14 12:55:05 -0500

    all moved to typescript!

--- helpers/deployHelpers.ts
+++ helpers/deployHelpers.ts
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const axios = require("axios");
+import dotenv from 'dotenv';
+import axios from "axios";
 
-const sleep = ms => {
+dotenv.config();
+
+const sleep = (ms: number) => {
   console.log(`sleeping ${ms/1000} seconds`);
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const waitForEtherscan = async (address, network) => {
+const waitForEtherscan = async (address: string, network: string) => {
+  console.log('waiting for the contract to be available on etherscan...')
   const { ETHERSCAN_API_KEY } = process.env;
   let deployed = false;
 
   while (!deployed) {
     const networkString = network == "ethereum" ? "" : `-${network}`;
     const apiToHit = `https://api${networkString}.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${ETHERSCAN_API_KEY}`;
-    console.log(apiToHit)
     const res = await axios.get(apiToHit);
     if (res.data.status == 1) {
       console.log("contract code finally verified");
@@ -25,6 +27,6 @@ const waitForEtherscan = async (address, network) => {
   }
 }
 
-  module.exports = {
+  export {
    waitForEtherscan, 
   }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,8 +3,6 @@ import "@nomiclabs/hardhat-ethers";
 import { waitForEtherscan } from "../helpers/deployHelpers";
 const [filePath] = process.argv.slice(2);
 
-console.log('filepath', filePath)
-
 interface ContractInfo {
   contractName: string;
   contractArgs: Array<any>;
@@ -13,31 +11,29 @@ interface ContractInfo {
 const getContractInfo = async (file: string): Promise<ContractInfo> => await import(`../${file}`);
 
 async function main() {
-  console.log('filepath', filePath)
 
   const { contractName, contractArgs } = await getContractInfo(filePath);
-  // console.log('data', data)
 
-  console.log('contractname', contractName)
-  console.log('contractArgs', contractArgs)
+  console.log('contract name:', contractName)
+  console.log('contract args:', contractArgs)
+
   await run("compile");
   
   const ERC721Factory = await ethers.getContractFactory(contractName);
   const contractInstance = await ERC721Factory.deploy(...contractArgs); // Instance of the contract 
 
-  console.log(`deploying contract: ${contractInstance.address} to the ${network.name} network`);
+  console.log(`deploying contract: ${contractInstance.address} to the ${network.name} network...`);
   await contractInstance.deployed();
-  console.log('contract deployed:', contractInstance.address);
+  console.log('contract deployed');
 
   await waitForEtherscan(contractInstance.address, network.name)
 
+  console.log('verifying the contract on etherscan...');
   await run("verify:verify", {
     address: contractInstance.address,
     constructorArguments: contractArgs,
   });
-
  }
-
  
  main()
    .then(() => process.exit(0))
